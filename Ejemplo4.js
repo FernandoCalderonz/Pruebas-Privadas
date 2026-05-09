e786d2e93c87561a4a3fd5a6182da17bd1372fb3
Fernando Calderon <luckfury6@gmail.com>
2026-05-08 23:38:06 -0500

    add Ejemplo4

---
+++ Ejemplo4.js
@@ -0,0 +1,8 @@
+function saludarcomunidad(nombrecomunidad){
+    console.log("bienvenida comunidad:" + nombrecomunidad);
+    console.log("Hoy seguimos aprendiendo javascript paso a paso");
+    console.log("fuerte abraso");
+}
+
+saludarcomunidad("programadores para la paz");
+saludarcomunidad("juventudes");
